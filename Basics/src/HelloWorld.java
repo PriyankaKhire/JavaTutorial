d57fcc3ef7fc52e6f273803f56c476efc2781057
Priyanka Khire <priyanka.khire@gmail.com>
2024-03-07 18:01:18 -0800

    Added Hello World

---
+++ Basics/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+
+    /**
+     We always need String[] args for this to run
+     **/
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
